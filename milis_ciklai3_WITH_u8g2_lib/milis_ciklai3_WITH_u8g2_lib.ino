216bd41ff086ce62c80bbb86657fb8cc32c5ba25
EdenLT <makleriux@gmail.com>
2018-12-03 17:11:29 +0000

    Hydro w u8g2
    Hydroponic
    u8g2 lib
    milis for minutes and hours online

--- milis_ciklai3_WITH_u8g2_lib/milis_ciklai3_WITH_u8g2_lib.ino
+++ milis_ciklai3_WITH_u8g2_lib/milis_ciklai3_WITH_u8g2_lib.ino
@@ -8,24 +8,20 @@ DS12b20 pin = D3
 #include <DallasTemperature.h> //DS18B20 waterproof temp sensor 
 #include <DHT.h> // DTH22 temp, RH sensor
 //LCD SSD1306
-//#include <SPI.h>
+#include <Arduino.h>
+#include <U8g2lib.h>
+#include <SPI.h>
 #include <Wire.h>
-//#include <Adafruit_GFX.h>
-//#include <Adafruit_SSD1306.h>
-#include <U8glib.h> //TEMP TEST
 
+U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+//U8G2_SSD1306_128X64_NONAME_1_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);   // All Boards without Reset of the Display
 //-----------------------------------------  set pin numbers:
 #define MUX_Address 0x70 // TCA9548A Encoders address
 #define ONE_WIRE_BUS 3 // Data wire for DS18B20
 #define DHTPIN 2     // DTH 22 what pin we're connected to
 #define DHTTYPE DHT22   // DHT 22  (AM2302)
 DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
-//LCD SSD1306
-/*#define OLED_RESET A4
-Adafruit_SSD1306 display1(OLED_RESET); //1st screen
-Adafruit_SSD1306 display2(OLED_RESET); //2nd screen
-#define NUMFLAKES 10
-*///setings
+//setings
 int relay_1 = 5; //relay
 int targetHumidity = 50;  //RH
 //DS18B20 
@@ -48,8 +44,7 @@ float temp_1 = 0;
 float temp_2 = 0;
 float temp_3 = 0;
 char tmp_string[8];  // Temp string to convert numeric values to string before print to OLED display
-U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_FAST);  // Fast I2C / TWI
-//I2C MUX
+//I2C MUX **********************************************************
 void tcaselect(uint8_t i2c_bus) {
     if (i2c_bus > 7) return;
     Wire.beginTransmission(MUX_Address);
@@ -57,27 +52,22 @@ void tcaselect(uint8_t i2c_bus) {
     Wire.endTransmission(); 
 }
 
-
+//******************************************************************
 void setup() {
-     Serial.begin(9600);
+  DisplayInit(); // Initialize the displays 
+     Serial.begin(9600);   
      sensors.begin(); //DS18B20
      dht.begin();   // DTH 22
-//LCD SSD1306
-DisplayInit(); // Initialize the displays 
-//display1.begin(SSD1306_SWITCHCAPVCC, 0x70);
-//display2.begin(SSD1306_SWITCHCAPVCC, 0x70);
 pinMode(relay_1,OUTPUT);//rele
 }
-//DISPLAY SETUP
+// Initialize the displays 
 void DisplayInit(){
-    for (int i = 0; i < 7; i++) {
-      tcaselect(i);   // Loop through each connected displays on the I2C buses  
-      u8g.firstPage();
-      do {
-        u8g.begin();  // Initialize display
-       } while( u8g.nextPage() );
-    }
-}
+    for (int i = 0; i < 7; i++) { // get through all MUX ports
+      tcaselect(i);   // Loop through each connected displays on the I2C buses       
+        u8g2.begin();  // Initialize display
+        u8g2.setFont(u8g2_font_profont11_tr); //LCD font
+}}
+//******************************************************************
 void loop()
 {
   unsigned long mainCycleCurrentMillis = millis(); // cycle starts
@@ -101,102 +91,59 @@ void loop()
     Serial.print(" %, Temp: ");
     Serial.print(dhtTemp);
     Serial.println(" Celsius"); 
-// //LCD SSD1306
-     /*
-     display1.clearDisplay();
-     lcdText("Vanduo ", 47, 0, 1, false);
-     lcdText("Temperatura: ", 0, 10, 1, false);
-     lcdText(tempToStr1, 75, 10, 1, false);
-     display1.display();
-     delay(200);
-     tcaselect(1);    
-     display2.clearDisplay();
-     lcd2Text("Boxas", 50, 0, 1, false);
-     lcd2Text("Dregnumas: ", 0, 10, 1, false);
-     lcd2Text(tempToStr3, 65, 10, 1, false);
-     lcd2Text("Temperatura: ", 0, 23, 1, false);
-     lcd2Text(tempToStr2, 75, 23, 1, false);
-     display2.display();
-     delay(200);
-*/
-   tcaselect(0);
-
-      u8g.firstPage();
+//LCD SSD1306
+tcaselect(0); // Selecting channel in MUX
+      u8g2.firstPage();  
       do {
         /******** Display Something *********/
-        u8g.setFont(u8g_font_ncenB10);
-        u8g.drawStr(0, 13, " to:");
-
-        itoa(0, tmp_string, 10);  
-        u8g.setFont(u8g_font_ncenB18);
-        u8g.drawStr(25, 50, tmp_string);
+    u8g2.drawStr(0,7,"1st Screen");
+    u8g2.drawStr(0,17, "Minutes Online: ");
+    u8g2.setCursor(95,17);
+    u8g2.print(cycle);
+    u8g2.drawStr(0,28," The quick brown fox");
+    u8g2.drawStr(0,35,"jumps over a lazy ");
+    u8g2.drawStr(0,42,"dog ");
         /************************************/
-      } while( u8g.nextPage() );
-      delay(50);
-tcaselect(1);
-
-      u8g.firstPage();
+      } while( u8g2.nextPage() );
+      delay(500);
+      
+tcaselect(1); // Selecting channel in MUX
+      u8g2.firstPage();  
       do {
         /******** Display Something *********/
-        u8g.setFont(u8g_font_ncenB10);
-        u8g.drawStr(0, 13, "DADA");
-
-        itoa(1, tmp_string, 10);  
-        u8g.setFont(u8g_font_ncenB18);
-        u8g.drawStr(25, 50, tmp_string);
+    u8g2.drawStr(0,7,"2nd Screen");
+    u8g2.drawStr(0,14," all good me to come");
+    u8g2.drawStr(0,21,"to the aid the party");
+    u8g2.drawStr(0,28," The quick brown fox");
+    u8g2.drawStr(0,35,"jumps over a lazy ");
+    u8g2.drawStr(0,42,"dog ");
         /************************************/
-      } while( u8g.nextPage() );
-      delay(50);
-   tcaselect(2);
-
-      u8g.firstPage();
+      } while( u8g2.nextPage() );
+      delay(500);
+tcaselect(2); // Selecting channel in MUX
+     u8g2.firstPage();  
       do {
         /******** Display Something *********/
-        u8g.setFont(u8g_font_ncenB10);
-        u8g.drawStr(0, 13, "Connected");
-
-        itoa(1, tmp_string, 10);  
-        u8g.setFont(u8g_font_ncenB18);
-        u8g.drawStr(25, 50, tmp_string);
+    u8g2.drawStr(0,7,"3rd Screen");
+    u8g2.drawStr(0,14," all good me to come");
+    u8g2.drawStr(0,21,"to the aid the party");
+    u8g2.drawStr(0,28," The quick brown fox");
+    u8g2.drawStr(0,35,"jumps over a lazy ");
+    u8g2.drawStr(0,42,"dog ");
         /************************************/
-      } while( u8g.nextPage() );
-      delay(50);
-    
+      } while( u8g2.nextPage() );
+      delay(500);
+      
     if (dhtHum < targetHumidity) {
        digitalWrite(relay_1, HIGH);
       }
       else { digitalWrite(relay_1, LOW);}
-    
-    cycle++; // cycle counter
-    mainCycleMillis = mainCycleCurrentMillis; // reset cycle timer
+      /******** Counters ***************/
+   if (cycle < 10 ){ 
+    cycle++; }
+    else {hourCycle++;
+    cycle = 0;}// cycle counter reset
+ mainCycleMillis = mainCycleCurrentMillis; // reset cycle timer
   }
 
 }
-/*
- * lcdText(String text, int x, int y,int size, boolean d)
- * text is the text string to be printed
- * x is the integer x position of text
- * y is the integer y position of text
- * z is the text size, 1, 2, 3 etc
- * d is either "true" or "false". Not sure, use true
- */
- /*
-void lcdText(String text, int x, int y,int size, boolean d) {
-  display1.setTextSize(size);
-  display1.setTextColor(WHITE);
-  display1.setCursor(x,y);
-  display1.println(text);
-  if(d){
-    display1.display();
-  }
-  delay(100);
-}
-void lcd2Text(String text, int x, int y,int size, boolean d) {
-  display2.setTextSize(size);
-  display2.setTextColor(WHITE);
-  display2.setCursor(x,y);
-  display2.println(text);
-  if(d){
-    display2.display();
-  }
-  */
--- milis_ciklai3_WITH_u8g_lib/milis_ciklai3_WITH_u8g_lib.ino
+++ milis_ciklai3_WITH_u8g_lib/milis_ciklai3_WITH_u8g_lib.ino
@@ -8,11 +8,8 @@ DS12b20 pin = D3
 #include <DallasTemperature.h> //DS18B20 waterproof temp sensor 
 #include <DHT.h> // DTH22 temp, RH sensor
 //LCD SSD1306
-//#include <SPI.h>
 #include <Wire.h>
-//#include <Adafruit_GFX.h>
-//#include <Adafruit_SSD1306.h>
-#include <U8glib.h> //TEMP TEST
+#include <U8glib.h> //LCD LIB
 
 //-----------------------------------------  set pin numbers:
 #define MUX_Address 0x70 // TCA9548A Encoders address
@@ -20,12 +17,7 @@ DS12b20 pin = D3
 #define DHTPIN 2     // DTH 22 what pin we're connected to
 #define DHTTYPE DHT22   // DHT 22  (AM2302)
 DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
-//LCD SSD1306
-/*#define OLED_RESET A4
-Adafruit_SSD1306 display1(OLED_RESET); //1st screen
-Adafruit_SSD1306 display2(OLED_RESET); //2nd screen
-#define NUMFLAKES 10
-*///setings
+//setings
 int relay_1 = 5; //relay
 int targetHumidity = 50;  //RH
 //DS18B20 
@@ -47,27 +39,26 @@ float dhtTemp; //Stores temperature value
 float temp_1 = 0;
 float temp_2 = 0;
 float temp_3 = 0;
+//LCD SSD1306
 char tmp_string[8];  // Temp string to convert numeric values to string before print to OLED display
 U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_FAST);  // Fast I2C / TWI
-//I2C MUX
+//I2C MUX **********************************************************
 void tcaselect(uint8_t i2c_bus) {
     if (i2c_bus > 7) return;
     Wire.beginTransmission(MUX_Address);
     Wire.write(1 << i2c_bus);
     Wire.endTransmission(); 
 }
-
-
+//******************************************************************
 void setup() {
      Serial.begin(9600);
      sensors.begin(); //DS18B20
      dht.begin();   // DTH 22
 //LCD SSD1306
 DisplayInit(); // Initialize the displays 
-//display1.begin(SSD1306_SWITCHCAPVCC, 0x70);
-//display2.begin(SSD1306_SWITCHCAPVCC, 0x70);
 pinMode(relay_1,OUTPUT);//rele
 }
+//******************************************************************
 //DISPLAY SETUP
 void DisplayInit(){
     for (int i = 0; i < 7; i++) {
@@ -102,66 +93,43 @@ void loop()
     Serial.print(dhtTemp);
     Serial.println(" Celsius"); 
 // //LCD SSD1306
-     /*
-     display1.clearDisplay();
-     lcdText("Vanduo ", 47, 0, 1, false);
-     lcdText("Temperatura: ", 0, 10, 1, false);
-     lcdText(tempToStr1, 75, 10, 1, false);
-     display1.display();
-     delay(200);
-     tcaselect(1);    
-     display2.clearDisplay();
-     lcd2Text("Boxas", 50, 0, 1, false);
-     lcd2Text("Dregnumas: ", 0, 10, 1, false);
-     lcd2Text(tempToStr3, 65, 10, 1, false);
-     lcd2Text("Temperatura: ", 0, 23, 1, false);
-     lcd2Text(tempToStr2, 75, 23, 1, false);
-     display2.display();
-     delay(200);
-*/
-   tcaselect(0);
-
+tcaselect(0); // Selecting channel in MUX
       u8g.firstPage();
       do {
         /******** Display Something *********/
         u8g.setFont(u8g_font_ncenB10);
-        u8g.drawStr(0, 13, " to:");
-
-        itoa(0, tmp_string, 10);  
+        u8g.drawStr(0, 13, "1st screen"); // X, Y static words 
+        itoa(0, tmp_string, 10);  // get string
         u8g.setFont(u8g_font_ncenB18);
-        u8g.drawStr(25, 50, tmp_string);
+        u8g.drawStr(25, 50, tmp_string); // X, Y draw string
         /************************************/
       } while( u8g.nextPage() );
       delay(50);
-tcaselect(1);
-
+tcaselect(1); // Selecting channel in MUX
       u8g.firstPage();
       do {
         /******** Display Something *********/
         u8g.setFont(u8g_font_ncenB10);
-        u8g.drawStr(0, 13, "DADA");
-
+        u8g.drawStr(0, 13, "2nd screen");
         itoa(1, tmp_string, 10);  
         u8g.setFont(u8g_font_ncenB18);
         u8g.drawStr(25, 50, tmp_string);
         /************************************/
       } while( u8g.nextPage() );
       delay(50);
-   tcaselect(2);
-
+tcaselect(2); // Selecting channel in MUX
       u8g.firstPage();
       do {
         /******** Display Something *********/
         u8g.setFont(u8g_font_ncenB10);
-        u8g.drawStr(0, 13, "Connected");
-
+        u8g.drawStr(0, 13, "3rd screen");
         itoa(1, tmp_string, 10);  
         u8g.setFont(u8g_font_ncenB18);
         u8g.drawStr(25, 50, tmp_string);
         /************************************/
       } while( u8g.nextPage() );
       delay(50);
-    
+      
     if (dhtHum < targetHumidity) {
        digitalWrite(relay_1, HIGH);
       }
@@ -172,31 +140,3 @@ tcaselect(1);
   }
 
 }
-/*
- * lcdText(String text, int x, int y,int size, boolean d)
- * text is the text string to be printed
- * x is the integer x position of text
- * y is the integer y position of text
- * z is the text size, 1, 2, 3 etc
- * d is either "true" or "false". Not sure, use true
- */
- /*
-void lcdText(String text, int x, int y,int size, boolean d) {
-  display1.setTextSize(size);
-  display1.setTextColor(WHITE);
-  display1.setCursor(x,y);
-  display1.println(text);
-  if(d){
-    display1.display();
-  }
-  delay(100);
-}
-void lcd2Text(String text, int x, int y,int size, boolean d) {
-  display2.setTextSize(size);
-  display2.setTextColor(WHITE);
-  display2.setCursor(x,y);
-  display2.println(text);
-  if(d){
-    display2.display();
-  }
-  */
